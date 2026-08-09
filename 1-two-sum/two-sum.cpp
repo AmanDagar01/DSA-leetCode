eac13038252be7853b9a7ff8fb48f0f9127c2050
Aman Dagar <120033508+AmanDagar01@users.noreply.github.com>
2026-08-09 13:20:21 +0530

    Time: 43 ms (24.39%) | Memory: 14.2 MB (72.20%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,13 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         for(int i=0; i<size(nums); i++){
             for(int j=i+1; j<size(nums); j++){
-                if(i==j){
-                }
-                else{
-                    if(nums[i]+nums[j]==target){
+                if(nums[i]+nums[j]==target){
                         return {i, j};
                     };
-                }
             }
         }
         return {};
